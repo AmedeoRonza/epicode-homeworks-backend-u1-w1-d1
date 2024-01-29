bfe056503fbbdb1b8ef6b293509384eae64d3c08
Amedeo Ronza <amedeoronza@gmail.com>
2024-01-29 20:43:10 +0100

    Gruppo2 classe fatta

---
+++ ConsoleApp1/Gruppo2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Gruppo2
+    {
+        private string _name;
+        private string _surname;
+        private string _valutazione;
+
+        public string Name
+        { get { return _name; } set {  _name = value; } }
+        public string Surname
+        { get { return _surname; } set { _surname = value; } }
+        public string Valutazione
+        { get { return _valutazione; } set { _valutazione = value; } }  
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,8 +22,9 @@ namespace ConsoleApp1
             Animali Animale3 = new Animali();
             Animale3.Name = "Palla di neve";
             Animale3.Razza = "Calamaro gigante";
-            Animale3.Habitat = "Alaska";
+            Animale3.Habitat = "Alaska"; 
 
+            Console.WriteLine("ANIMALI: " + "\n" + "\n");
             Console.WriteLine("Primo animale: " + "\n" + "\n" + "Nome: " + Animale1.Name + "\n");
             Console.WriteLine("Razza: " + Animale1.Razza + "\n");
             Console.WriteLine("Habitat: " + Animale1.Habitat + "\n" + "\n");
@@ -31,7 +33,44 @@ namespace ConsoleApp1
             Console.WriteLine("Habitat: " + Animale2.Habitat + "\n" + "\n");
             Console.WriteLine("Terzo animale: " + "\n" + "\n" + "Nome: " + Animale3.Name + "\n");
             Console.WriteLine("Razza: " + Animale3.Razza + "\n");
-            Console.WriteLine("Habitat: " + Animale3.Habitat);
+            Console.WriteLine("Habitat: " + Animale3.Habitat + "\n" + "\n");
+
+            Gruppo2 Membro1 = new Gruppo2();
+            Membro1.Name = "Amedeo";
+            Membro1.Surname = "Ronza";
+            Membro1.Valutazione = "10";
+            Gruppo2 Membro2 = new Gruppo2();
+            Membro2.Name = "Melissa";
+            Membro2.Surname = "Canu";
+            Membro2.Valutazione = "10";
+            Gruppo2 Membro3 = new Gruppo2();
+            Membro3.Name = "Egidio";
+            Membro3.Surname = "Roseti";
+            Membro3.Valutazione = "10";
+            Gruppo2 Membro4 = new Gruppo2();
+            Membro4.Name = "Francesco";
+            Membro4.Surname = "Napoli";
+            Membro4.Valutazione = "10";
+            Gruppo2 Membro5 = new Gruppo2();
+            Membro5.Name = "Antonio";
+            Membro5.Surname = "Marucci";
+            Membro5.Valutazione = "10";
+            Console.WriteLine("GRUPPO 2: " + "\n" + "\n");
+            Console.WriteLine("Primo Membro: " + "\n" + "\n" + "Nome: " + Membro1.Name + "\n");
+            Console.WriteLine("Cognome: " + Membro1.Surname + "\n");
+            Console.WriteLine("Valutazione: " + Membro1.Valutazione + "\n" + "\n");
+            Console.WriteLine("Secondo Membro: " + "\n" + "\n" + "Nome: " + Membro2.Name + "\n");
+            Console.WriteLine("Cognome: " + Membro2.Surname + "\n");
+            Console.WriteLine("Valutazione: " + Membro2.Valutazione + "\n" + "\n");
+            Console.WriteLine("Terzo Membro: " + "\n" + "\n" + "Nome: " + Membro3.Name + "\n");
+            Console.WriteLine("Cognome: " + Membro3.Surname + "\n");
+            Console.WriteLine("Valutazione: " + Membro3.Valutazione + "\n" + "\n");
+            Console.WriteLine("Quarto Membro: " + "\n" + "\n" + "Nome: " + Membro4.Name + "\n");
+            Console.WriteLine("Cognome: " + Membro4.Surname + "\n");
+            Console.WriteLine("Valutazione: " + Membro4.Valutazione + "\n" + "\n");
+            Console.WriteLine("Quinto Membro: " + "\n" + "\n" + "Nome: " + Membro5.Name + "\n");
+            Console.WriteLine("Cognome: " + Membro5.Surname + "\n");
+            Console.WriteLine("Valutazione: " + Membro5.Valutazione);
             Console.ReadLine();
         }
     }
