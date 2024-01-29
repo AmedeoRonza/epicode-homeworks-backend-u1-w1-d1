254884f36267412f7e7db16fd4855ba57d9fd2ea
Amedeo Ronza <amedeoronza@gmail.com>
2024-01-29 19:55:00 +0100

    Prima Classe fatta

---
+++ ConsoleApp1/Animali.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Animali
+    {
+        private string _name;
+        private string _razza;
+        private string _habitat;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Razza
+        {
+            get
+            {
+                return _razza;
+            }
+            set
+            {
+                _razza = value;
+            }
+        }
+            
+            public string Habitat
+        {
+            get
+            {
+                return _habitat;
+            }
+            set {
+                _habitat = value;
+            }
+        }
+    }
+}
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Animali Animale1 = new Animali();
+            Animale1.Name = "Felix";
+            Animale1.Razza = "Gatto";
+            Animale1.Habitat = "Domestico";
+            Animali Animale2 = new Animali();
+            Animale2.Name = "Bobby";
+            Animale2.Razza = "Cane";
+            Animale2.Habitat = "Domestico";
+            Animali Animale3 = new Animali();
+            Animale3.Name = "Palla di neve";
+            Animale3.Razza = "Calamaro gigante";
+            Animale3.Habitat = "Alaska";
+
+            Console.WriteLine("Primo animale: " + "\n" + "\n" + "Nome: " + Animale1.Name + "\n");
+            Console.WriteLine("Razza: " + Animale1.Razza + "\n");
+            Console.WriteLine("Habitat: " + Animale1.Habitat + "\n" + "\n");
+            Console.WriteLine("Secondo animale: " + "\n" + "\n" + "Nome: " + Animale2.Name + "\n");
+            Console.WriteLine("Razza: " + Animale2.Razza + "\n");
+            Console.WriteLine("Habitat: " + Animale2.Habitat + "\n" + "\n");
+            Console.WriteLine("Terzo animale: " + "\n" + "\n" + "Nome: " + Animale3.Name + "\n");
+            Console.WriteLine("Razza: " + Animale3.Razza + "\n");
+            Console.WriteLine("Habitat: " + Animale3.Habitat);
+            Console.ReadLine();
         }
     }
 }
